9e9b42447c2f9ee3d8278a9f1407f08fcc46cfc5
rimputapacimbunabadezileaka <101021416+rimputapacimbunabadezileaka@users.noreply.github.com>
2022-03-21 17:09:33 +0100

    Reduced HTML structure
    Reduced HTML structure, cleaner code for board and part of pieces imported.

--- app.js
+++ app.js
@@ -1,2 +1,22 @@
-const rookA8 = document.getElementById("a8");
-const a4 = document.getElementById("h4");
+//generate chess board
+for (let i = 0; i < 64; i++) {
+  document
+    .getElementById("mainChessBoard")
+    .appendChild(document.createElement("div")).style.backgroundColor =
+    parseInt(i / 8 + i) % 2 === 0 ? "#F6F7EB" : "#8693AB";
+}
+
+//selectring all squares as nodelist
+const allSquares = document.querySelectorAll("#mainChessBoard > div");
+
+console.log(allSquares);
+
+for (let i = 0; i < allSquares.length; i++) {
+  let chessPieceImg = document.createElement("img");
+  let imgUrl = "./chessPieceSet-pngs/" + i + ".png";
+
+  if (imgUrl.statusCode !== "404") {
+    chessPieceImg.src = imgUrl;
+    allSquares[i].appendChild(chessPieceImg);
+  }
+}
