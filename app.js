709622ab2a666b6ececd244414b26ffd423b98a9
rimputapacimbunabadezileaka <101021416+rimputapacimbunabadezileaka@users.noreply.github.com>
2022-03-21 19:44:06 +0100

    Starting position
    Starting position ready

--- app.js
+++ app.js
@@ -9,14 +9,71 @@ for (let i = 0; i < 64; i++) {
 //selectring all squares as nodelist
 const allSquares = document.querySelectorAll("#mainChessBoard > div");
 
-console.log(allSquares);
-
+//setting up the pieces on the board (starting position)
 for (let i = 0; i < allSquares.length; i++) {
-  let chessPieceImg = document.createElement("img");
-  let imgUrl = "./chessPieceSet-pngs/" + i + ".png";
+  const chessPieceImg = document.createElement("img");
+  const blackPiecesUrl = "./chessPieceSet-pngs/black";
+  const whitePiecesUrl = "./chessPieceSet-pngs/white";
 
-  if (imgUrl.statusCode !== "404") {
-    chessPieceImg.src = imgUrl;
+  //adding black pieces
+  //adding rooks
+  if (i === 0 || i === 7) {
+    chessPieceImg.src = blackPiecesUrl + "Rook.png";
+    allSquares[i].appendChild(chessPieceImg);
+
+    //adding knights
+  } else if (i === 1 || i === 6) {
+    chessPieceImg.src = blackPiecesUrl + "Knight.png";
+    allSquares[i].appendChild(chessPieceImg);
+
+    //adding bishops
+  } else if (i === 2 || i === 5) {
+    chessPieceImg.src = blackPiecesUrl + "Bishop.png";
+    allSquares[i].appendChild(chessPieceImg);
+
+    //adding queen
+  } else if (i === 3) {
+    chessPieceImg.src = blackPiecesUrl + "Queen.png";
+    allSquares[i].appendChild(chessPieceImg);
+
+    //adding king
+  } else if (i === 4) {
+    chessPieceImg.src = blackPiecesUrl + "King.png";
+    allSquares[i].appendChild(chessPieceImg);
+
+    //adding pawns
+  } else if (i > 7 && i < 16) {
+    chessPieceImg.src = blackPiecesUrl + "Pawn.png";
+    allSquares[i].appendChild(chessPieceImg);
+  }
+
+  //adding white pieces
+  //adding pawns
+  if (i > 47 && i < 56) {
+    chessPieceImg.src = whitePiecesUrl + "Pawn.png";
+    allSquares[i].appendChild(chessPieceImg);
+  } else if (i === 56 || i === 63) {
+    chessPieceImg.src = whitePiecesUrl + "Rook.png";
+    allSquares[i].appendChild(chessPieceImg);
+
+    //adding knights
+  } else if (i === 57 || i === 62) {
+    chessPieceImg.src = whitePiecesUrl + "Knight.png";
+    allSquares[i].appendChild(chessPieceImg);
+
+    //adding bishops
+  } else if (i === 58 || i === 61) {
+    chessPieceImg.src = whitePiecesUrl + "Bishop.png";
+    allSquares[i].appendChild(chessPieceImg);
+
+    //adding queen
+  } else if (i === 59) {
+    chessPieceImg.src = whitePiecesUrl + "Queen.png";
+    allSquares[i].appendChild(chessPieceImg);
+
+    //adding king
+  } else if (i === 60) {
+    chessPieceImg.src = whitePiecesUrl + "King.png";
     allSquares[i].appendChild(chessPieceImg);
   }
 }
