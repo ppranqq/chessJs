77391fe4b50f3ae37757398743439e9c978abc44
rimputapacimbunabadezileaka <101021416+rimputapacimbunabadezileaka@users.noreply.github.com>
2022-03-23 17:19:00 +0100

    Added Classes to Pieces
    Every chess piece has it's class.

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ for (let i = 0; i < 64; i++) {
 //selecting all squares as nodelist
 const allSquares = document.querySelectorAll("#mainChessBoard > div");
 
-//setting up the pieces on the board (starting position)
+//setting up the pieces on the board (starting position), also adding classes to pieces to make them unique by they rules of moving
 for (let i = 0; i < allSquares.length; i++) {
   const chessPieceImg = document.createElement("img");
   const blackPiecesUrl = "./chessPieceSet-pngs/black";
@@ -20,73 +20,75 @@ for (let i = 0; i < allSquares.length; i++) {
   if (i === 0 || i === 7) {
     chessPieceImg.src = blackPiecesUrl + "Rook.png";
     allSquares[i].appendChild(chessPieceImg);
+    chessPieceImg.className = "ROOK";
 
     //adding knights
   } else if (i === 1 || i === 6) {
     chessPieceImg.src = blackPiecesUrl + "Knight.png";
     allSquares[i].appendChild(chessPieceImg);
+    chessPieceImg.className = "KNIGHT";
 
     //adding bishops
   } else if (i === 2 || i === 5) {
     chessPieceImg.src = blackPiecesUrl + "Bishop.png";
     allSquares[i].appendChild(chessPieceImg);
+    chessPieceImg.className = "BISHOP";
 
     //adding queen
   } else if (i === 3) {
     chessPieceImg.src = blackPiecesUrl + "Queen.png";
     allSquares[i].appendChild(chessPieceImg);
+    chessPieceImg.className = "QUEEN";
 
     //adding king
   } else if (i === 4) {
     chessPieceImg.src = blackPiecesUrl + "King.png";
     allSquares[i].appendChild(chessPieceImg);
+    chessPieceImg.className = "KING";
 
     //adding pawns
   } else if (i > 7 && i < 16) {
     chessPieceImg.src = blackPiecesUrl + "Pawn.png";
     allSquares[i].appendChild(chessPieceImg);
+    chessPieceImg.className = "PAWN"; // very important, later on i might want to use different logic for white and black pawns, maybe use blackPAWN, whitePAWN
   }
 
-  //adding white pieces
+  //adding white pieces and classes as previously mentioned
   //adding pawns
   if (i > 47 && i < 56) {
     chessPieceImg.src = whitePiecesUrl + "Pawn.png";
     allSquares[i].appendChild(chessPieceImg);
+    chessPieceImg.className = "PAWN";
   } else if (i === 56 || i === 63) {
     chessPieceImg.src = whitePiecesUrl + "Rook.png";
     allSquares[i].appendChild(chessPieceImg);
+    chessPieceImg.className = "ROOK";
 
     //adding knights
   } else if (i === 57 || i === 62) {
     chessPieceImg.src = whitePiecesUrl + "Knight.png";
     allSquares[i].appendChild(chessPieceImg);
+    chessPieceImg.className = "KNIGHT";
 
     //adding bishops
   } else if (i === 58 || i === 61) {
     chessPieceImg.src = whitePiecesUrl + "Bishop.png";
     allSquares[i].appendChild(chessPieceImg);
+    chessPieceImg.className = "BISHOP";
 
     //adding queen
   } else if (i === 59) {
     chessPieceImg.src = whitePiecesUrl + "Queen.png";
     allSquares[i].appendChild(chessPieceImg);
+    chessPieceImg.className = "QUEEN";
 
     //adding king
   } else if (i === 60) {
     chessPieceImg.src = whitePiecesUrl + "King.png";
     allSquares[i].appendChild(chessPieceImg);
+    chessPieceImg.className = "KING";
   }
 }
 
 //selecting all img/pieces as node list
 const allPieces = document.querySelectorAll("#mainChessBoard > div > img");
-
-allPieces.forEach((piece) => {
-  piece.addEventListener("dragstart", () => {
-    piece.classList.add("startMovingPiece");
-  });
-
-  piece.addEventListener("dragend", () => {
-    piece.classList.remove("startMovingPiece");
-  });
-});
