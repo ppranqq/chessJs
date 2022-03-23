fe82c24dd4e87c252d85bebc21c018bf0ea4cf02
nfgCodex <nerdfoundrygaming+git@gmail.com>
2022-03-23 15:57:32 -0500

    Changes to make it actually run/operate

--- app.js
+++ app.js
@@ -1,28 +1,29 @@
-const URL_PIECES_COLOR1 = "./chessPieceSet-pngs/black";
-const URL_PIECES_COLOR2 = "./chessPieceSet-pngs/white";
+const URL_PIECES_COLOR1 = './chessPieceSet-pngs/black';
+const URL_PIECES_COLOR2 = './chessPieceSet-pngs/white';
 
 const CHECKER_COLOR = {
   EVEN: '#F6F7EB',
   ODD: '#8693AB'
 };
 
-const generateChessBoard = (mainChessBoard) => {
+const generateChessBoard = mainChessBoard => {
   //generate chess board
   for (let i = 0; i < 64; i++) {
-    mainChessBoard.appendChild(document.createElement("div")).style.backgroundColor =
+    mainChessBoard.appendChild(document.createElement('div')).style.backgroundColor =
       0 === parseInt(i / 8 + i) % 2 ? CHECKER_COLOR.EVEN : CHECKER_COLOR.ODD;
   }
 
-  return mainChessBoard.querySelectorAll("> div");
-}
+  return document.querySelectorAll('#mainChessBoard > div');
+};
 
-const layoutInitialChessPieces = (allSquares) => {
-  let chessPieceImg, pieceName, chosenColor;
+const layoutInitialChessPieces = allSquares => {
+  let chessPieceImg, chosenColor;
 
   //setting up the pieces on the board (starting position), also adding classes to pieces to make them unique by they rules of moving
   for (let i = 0; i < allSquares.length; i++) {
-    chosenColor = i < 50 ? URL_PIECES_COLOR1 : URL_PIECES_COLOR2;
-    chessPieceImg = document.createElement("img");
+    let pieceName;
+    chosenColor = i < 48 ? URL_PIECES_COLOR1 : URL_PIECES_COLOR2;
+    chessPieceImg = document.createElement('img');
 
     switch (i) {
       case 0:
@@ -52,9 +53,9 @@ const layoutInitialChessPieces = (allSquares) => {
         pieceName = 'King';
         break;
       default:
-        // FIXME: Don't set if in an empty range
-        // if (i >= xx && i <= yy) {}
-        pieceName = 'Pawn'
+        if (i < 16 || i > 47) {
+          pieceName = 'Pawn';
+        }
         break;
     }
 
@@ -65,10 +66,11 @@ const layoutInitialChessPieces = (allSquares) => {
     }
   }
 
-  return mainChessBoard.querySelectorAll("> div > img");
-}
+  return document.querySelectorAll('#mainChessBoard > div > img');
+};
 
-let isMoving = false, movingIdx;
+let isMoving = false,
+  movingIdx;
 
 /** @param {HTMLElement[]} chessPieces */
 const startMovingPiece = (chessPieces, index) => {
@@ -76,24 +78,28 @@ const startMovingPiece = (chessPieces, index) => {
     return;
   }
 
-  chessPieces[index].classList.add("selectedPiece");
-
+  chessPieces[index].classList.add('selectedPiece');
   movingIdx = index;
   isMoving = true;
-}
+};
 
 /** @param {HTMLElement[]} chessPieces */
-const endMovingPiece = (chessPieces, index) => {
-  chessPieces[movingIdx].classList.remove("selectedPiece");
-  chessPieces[index].appendChild(chessPieces[movingIdx]);
+const endMovingPiece = (chessSquares, chessPieces, index) => {
+  if (!isMoving) {
+    return;
+  }
 
-  isMoving = false
+  chessPieces[movingIdx].classList.remove('selectedPiece');
+  chessSquares[index].appendChild(chessPieces[movingIdx]);
+  isMoving = false;
   movingIdx = -1;
-}
+};
 
-const mainChessBoard = document.getElementById("mainChessBoard");
+const mainChessBoard = document.getElementById('mainChessBoard');
 const chessSquares = generateChessBoard(mainChessBoard);
-const chessPieces = layoutInitialChessPieces(mainChessBoard, chessSquares);
+const chessPieces = layoutInitialChessPieces(chessSquares);
 
-chessPieces.forEach((piece, idx) => piece.addEventListener('click', startMovingPiece.bind(null, chessPieces, idx)))
-chessSquares.forEach((square, idx) => square.addEventListener('click', endMovingPiece.bind(null, chessPieces, idx)))
+chessSquares.forEach((square, idx) =>
+  square.addEventListener('click', endMovingPiece.bind(null, chessSquares, chessPieces, idx), true)
+);
+chessPieces.forEach((piece, idx) => piece.addEventListener('click', startMovingPiece.bind(null, chessPieces, idx)));
